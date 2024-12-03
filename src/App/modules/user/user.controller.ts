8ff662eefc05e8bac666c3e8ef9a896e60def224
franco <francojmprogramacion@gmail.com>
2024-12-03 10:29:44 -0300

    optional gateway

--- src/App/modules/user/user.controller.ts
+++ src/App/modules/user/user.controller.ts
@@ -8,10 +8,13 @@ export class UserController {
   @Post('')
   @HttpCode(HttpStatus.OK)
   @ApiOperation({description:"Test endpoint: Login user"})
-  async login() {
-
-    const response = await axios.post(process.env.AWS_GATEWAY);
+  async login() { 
+    const gatewayUrl = process.env.AWS_GATEWAY;
+    if (gatewayUrl) {
+    const response = await axios.post(gatewayUrl);
     console.log(response.data);
+    }
+
 
     return { statusCode: HttpStatus.OK, result: 'login' };
   }
